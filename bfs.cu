6bf5919729be8336e94f4cc4e10931f7c3eccd1a
Pietro Polinari Cavassin <ppc19@inf.ufpr.br>
2022-09-18 12:11:00 -0300

    generate device graph

--- bfs.cu
+++ bfs.cu
@@ -14,84 +14,125 @@
 
 #define GPU_NAME "GTX750-Ti"
 #define MP 5
-#define THREADS_PER_BLOCK 1024   // can be defined in the compilation line with -D
+#define THREADS_PER_BLOCK 1024
 #define THREADS_PER_MP 2048
 #define RESIDENT_BLOCKS_PER_MP THREADS_PER_MP/THREADS_PER_BLOCK
-#define NTA \
-  (MP * RESIDENT_BLOCKS_PER_MP * THREADS_PER_BLOCK)
+#define NTA (MP * RESIDENT_BLOCKS_PER_MP * THREADS_PER_BLOCK)
 
-
-// void copy_from_device(uint32_t *hostArr, uint32_t *devArr, long size){
-//   //printf("Copy output data from the CUDA device to the host memory\n");
-
-//   cudaError_t err = cudaSuccess;
-//   err = cudaMemcpy(hostArr, devArr, size, cudaMemcpyDeviceToHost);
-
-//   if (err != cudaSuccess)
-//   {
-//       fprintf(stderr, "Failed to copy vector C from device to host (error code %s)!\n", cudaGetErrorString(err));
-//       exit(EXIT_FAILURE);
-//   }
-// }
+#define HOS2DEV cudaMemcpyHostToDevice
+#define DEV2HOS cudaMemcpyDeviceToHost
 
 /*
- * Host main program
- */
-int main(int argc, char *argv[])
-{
+    Invólucro para cudaMemcpy para lidar com erros
+*/
+void copy_mem(uint32_t *srcArr, uint32_t *dstArr, long long numElements, int way){
+    cudaError_t err = cudaSuccess;
 
-    std::vector<std::pair<int, int>> edges;
-
-    int u, v, max_vert = 0;
-    while (std::cin >> u >> v){
-      edges.push_back({u, v});
-      max_vert = max(max_vert, u);
-      max_vert = max(max_vert, v);
-    }
-    max_vert++;
-
-    thrust::device_vector< thrust::device_vector<int> > g_dev(max_vert);
-
-    for (auto p : edges){
-        g_dev[p.first].push_back(p.second);
-        g_dev[p.second].push_back(p.first);
-    }
-
-    // thrust::host_vector< thrust::host_vector<int> > g_host(max_vert, thrust::device_vector<int>);
-
-/*
-    long numElements = atol(argv[1]);
-    int nblk = atoi(argv[2]);
-    char option = argv[3][0];
-
-void aloca_vetor_device(uint32_t numElements){
-    // Alocações
     size_t size = numElements * sizeof(uint32_t);
-    printf("Aloca vetor de %d elementos\n", numElements);
+    err = cudaMemcpy(dstArr, srcArr, size, way);
 
-    uint32_t *d_Out1 = NULL;
-    err = cudaMalloc((void **)&d_Out1, size);
-    if (err != cudaSuccess)
-    {
-        fprintf(stderr, "Failed to allocate device vector C (error code %s)!\n", cudaGetErrorString(err));
+    if (err != cudaSuccess){
+      fprintf(stderr, "Failed to copy vector C from device to host (error code %s)!\n", cudaGetErrorString(err));
+      exit(EXIT_FAILURE);
+    }
+}
+
+/*
+    Aloca vetor com numElements elementos no device
+*/
+uint32_t *new_device_array(uint32_t numElements){
+    cudaError_t err;
+
+    size_t size = numElements * sizeof(uint32_t);
+    printf("Alocando vetor de %d elementos\n", numElements);
+
+    uint32_t *d_arr = NULL;
+    err = cudaMalloc((void **)&d_arr, size);
+    if (err != cudaSuccess){
+        fprintf(stderr, "Falha ao alocar vetor (erro %s)!\n", cudaGetErrorString(err));
         exit(EXIT_FAILURE);
     }
 
-    uint32_t *h_Out1 = (uint32_t *) malloc(size);
-    if (!h_Out1 ){
-      fprintf(stderr, "Error allocating local array!\n");
-      exit(1);
+    return d_arr;
+}
+
+/*
+    Aloca vetor com numElements elementos no host
+*/
+uint32_t *new_host_array(uint32_t numElements){
+    uint32_t *h_arr = (uint32_t *) malloc(size);
+
+    if (!h_arr ){
+        fprintf(stderr, "Falha ao alocar vetor local!\n");
+        exit(1);
     }
+}
+
+/*
+    Desaloca vetor no device
+*/
+void free_device_array(uint32_t *dev_array){
+
+    err = cudaFree(dev_array);
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Falha ao desalocar vetor (erro %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+}
+
+/*
+    Desaloca vetor no host
+*/
+void free_host_array(uint32_t *hos_array){
+    if (hos_array)
+        free(hos_array);
+}
+
+/*
+    Gera lista compacta a partir de lista de adjacências
+*/
+void gen_compact_list(uint32_t *vert, uint32_t *list, std::vector< std::vector<int> > &g){
+    int vert_i = 0, list_i = 0;
+    for (auto u : g){
+        vert[vert_i] = list_i;
+        for (auto v : g[u]) list[list_i++] = v;
+
+        vert_i++;
+    }
+
+    vert[vert_i] = list_i;
+}
+
+/*
+    Gera grafo no device a partir de vetor de arestas
+*/
+void gen_dev_graph(std::vector< std::pair<int,int> > &edges, 
+        uint32_t **g_vert_dev, uint32_t **g_list_dev, int vert_n){
+
+    std::vector< std::vector<int> > g_hos(vert_n);
+    for (auto p : edges){
+        g_hos[p.first].push_back(p.second);
+        g_hos[p.second].push_back(p.first);
+    }
+
+    uint32_t *g_vert_hos, *g_list_hos;
+    g_vert_hos = new_host_array( vert_n+1 );
+    g_list_hos = new_host_array( edges.size()*2+1 );
+
+    *g_vert_dev = new_device_array( vert_n+1 );
+    *g_list_dev = new_device_array( edges.size()*2+1 );
+
+    gen_compact_list(g_vert_hos, g_list_hos, g_hos);
+
+    copy_mem(g_vert_dev, g_vert_hos, vert_n, HOS2DEV);
+    copy_mem(g_list_dev, g_list_hos, vert_n, HOS2DEV);
+
+    free_host_array(g_vert_hos);
+    free_host_array(g_list_hos);
 
 }
 
-void aloca_grafo(std::vector< std::pair<int,int> > edges){
-
-
-    for (auto e : edges){
-
-    }
-}
 
 /*
  * Host main program
@@ -100,17 +141,19 @@ int main(int argc, char *argv[])
 {
 
     std::vector<std::pair<int, int>> edges;
-
-    int u, v, max_vert = 0;
-    int edge_num = 0;
+    int vert_n = 0, edge_n;
     while (std::cin >> u >> v){
-      edge_num++;
-      edges.push_back({u, v});
-      max_vert = max(max_vert, u);
-      max_vert = max(max_vert, v);
+        edges.push_back({u, v});
+        vert_n = max(vert_n, u);
+        vert_n = max(vert_n, v);
     }
+    vert_n++;
+    edge_n = edges.size();
 
-    max_vert++;
+    uint32_t *g_vert_dev, *g_list_dev;
+    gen_dev_graph(edges, g_vert_dev, g_list_dev, vert_n);
+
+
 /*
     long numElements = atol(argv[1]);
     int nblk = atoi(argv[2]);
@@ -186,15 +229,7 @@ int main(int argc, char *argv[])
 
     printf("Máximo: %u\n", res);
     printf("Tempo:  %.3fms\n", chrono_gettotal(&c3)/1000000.0);
-
-
-    // DEALLOC MEMORY
-    err = cudaFree(d_Out1);
-    if (err != cudaSuccess)
-    {
-        fprintf(stderr, "Failed to free device vector d_Out1 (error code %s)!\n", cudaGetErrorString(err));
-        exit(EXIT_FAILURE);
-    }*/
+    */
 
     return 0;
 }
