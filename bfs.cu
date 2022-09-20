c22c9c73e6ef62ccf47d3a466bcbdf3a7c1373a4
Pietro Polinari Cavassin <ppc19@inf.ufpr.br>
2022-09-20 16:31:35 -0300

    refactor code

--- bfs.cu
+++ bfs.cu
@@ -22,6 +22,17 @@
 #define DEV2HOS cudaMemcpyDeviceToHost
 
 /*
+    Função auxiliar para imprimir vetor
+*/
+void print_array(uint32_t *arr, int size){
+    for (int i = 0; i < size; ++i)
+        std::cout << arr[i] << " ";
+    std::cout << std::endl;
+}
+
+/************* INVÓLUCROS DE FUNÇÕES CUDA ************/
+
+/*
     Invólucro para cudaMemcpy para lidar com erros
 */
 void copy_mem(uint32_t *dstArr, uint32_t *srcArr, long long numElements, cudaMemcpyKind way){
@@ -35,6 +46,9 @@ void copy_mem(uint32_t *dstArr, uint32_t *srcArr, long long numElements, cudaMem
         exit(EXIT_FAILURE);
     }
 }
+
+
+/********** FUNÇÕES DE ALOCAÇÃO ***********/
 
 /*
     Aloca vetor com numElements elementos no device
@@ -71,15 +85,6 @@ uint32_t *new_host_array(uint32_t numElements){
 }
 
 /*
-    Função auxiliar para imprimir vetor
-*/
-void print_array(uint32_t *arr, int size){
-    for (int i = 0; i < size; ++i)
-        std::cout << arr[i] << " ";
-    std::cout << std::endl;
-}
-
-/*
     Desaloca vetor no device
 */
 void free_device_array(uint32_t *dev_array){
@@ -101,6 +106,8 @@ void free_host_array(uint32_t *hos_array){
         free(hos_array);
 }
 
+/************** GERAÇÃO DE GRAFO *****************/
+
 /*
     Gera lista compacta a partir de lista de adjacências
 */
@@ -109,10 +116,8 @@ void gen_compact_list(uint32_t *vert, uint32_t *list, std::vector< std::vector<i
     for (auto v_list : g){
         vert[vert_i] = list_i;
         for (auto v : v_list) list[list_i++] = v;
-
         vert_i++;
     }
-
     vert[vert_i] = list_i;
 }
 
@@ -120,44 +125,59 @@ void gen_compact_list(uint32_t *vert, uint32_t *list, std::vector< std::vector<i
     Gera grafo no device a partir de vetor de arestas
 */
 void gen_dev_graph(std::vector< std::pair<int,int> > &edges, 
-        uint32_t **g_vert_dev, uint32_t **g_list_dev, int vert_n){
+        uint32_t *g_vert_dev, uint32_t *g_list_dev, int vert_n, int edge_n){
+    // Aloca vetor de vértices e lista de adjacências no host
+    uint32_t *g_vert_hos, *g_list_hos;
+    g_vert_hos = new_host_array( vert_n+1 );
+    g_list_hos = new_host_array( edge_n*2+1 );
 
+    // Gera lista de adjacências no host
     std::vector< std::vector<int> > g_hos(vert_n);
     for (auto p : edges){
         g_hos[p.first].push_back(p.second);
         g_hos[p.second].push_back(p.first);
     }
 
-    uint32_t *g_vert_hos, *g_list_hos;
-    g_vert_hos = new_host_array( vert_n+1 );
-    g_list_hos = new_host_array( edges.size()*2+1 );
-
-    *g_vert_dev = new_device_array( vert_n+1 );
-    *g_list_dev = new_device_array( edges.size()*2+1 );
-
+    // Gera lista compacta de adjacências no host
     gen_compact_list(g_vert_hos, g_list_hos, g_hos);
 
-    copy_mem(*g_vert_dev, g_vert_hos, vert_n+1, HOS2DEV);
-    copy_mem(*g_list_dev, g_list_hos, edges.size()*2+1, HOS2DEV);
+    copy_mem(g_vert_dev, g_vert_hos, vert_n+1, HOS2DEV);
+    copy_mem(g_list_dev, g_list_hos, edge_n*2+1, HOS2DEV);
 
     free_host_array(g_vert_hos);
     free_host_array(g_list_hos);
 }
 
 
+/************* CÁLCULO DA BFS ************/
+
+/*
+    Kernel que processa todos os vértices da fronteira e atualiza os próximos
+    a serem processados
+        . g_vert: vetor de apontadores para início das listas de adjacências de cada vértice
+        . g_list: vetor com todas as listas de adjacências
+        . dist: vetor de distâncias dos vértices
+        . proc: vetor de vértices processados
+        . fron: vetor de vértices na fronteira
+        . vert_sz e list_sz: números de elementos de g_vert e g_list, respectivamente
+        . ended: informa se não há mais vértices a serem processados 
+*/
 __global__ 
-void process_frontier(
+void advance_frontier(
     uint32_t *g_vert, uint32_t *g_list, 
     uint32_t *dist, uint32_t *proc, uint32_t *fron, 
     int vert_sz, int list_sz, uint32_t *ended){
 
+    // Calcula índice da thread no total.
     int tid = threadIdx.x;
     int bid = blockIdx.x;
     int bdim = blockDim.x;
     int vertIdx = bid*bdim + tid;
 
+    // Garante que thread tem indice válido
     if (vertIdx >= vert_sz-1) return;
 
+    // Processa vértices na fronteira
     if (fron[vertIdx]){
         fron[vertIdx] = 0;
         proc[vertIdx] = 1;
@@ -169,10 +189,65 @@ void process_frontier(
         }
     }
 
+    // inicializa ended como 1
     if (vertIdx == 0) *ended = 1;
     __syncthreads();
+
+    // Se fronteira não estiver vazia, ended = 0
     if ( fron[vertIdx] ) *ended = 0;
 
+}
+
+/*
+    Inicializa valores iniciais dos vetores dist, proc e fron
+        . dist_dev corresponde às distâncias calculadas pela BFS
+        . proc_dev informa se cada vértice foi processado ou não
+        . fron_dev informa se cada vértice será processado na próxima iteração ou não
+*/
+void initialize_aux_arrays(uint32_t *dist_dev, uint32_t *proc_dev, uint32_t *fron_dev, int vert_n){
+    cudaMemset(dist_dev, 0xff, vert_n*sizeof(uint32_t)); // Distância começa com "infinito"
+    cudaMemset(proc_dev, 0, vert_n*sizeof(uint32_t));    // Nenhum vértice foi processado
+    cudaMemset(fron_dev, 0, vert_n*sizeof(uint32_t));    // Nenhum vértice está na fronteira
+
+    int val;
+    // Primeiro vértice tem distância 0 e pertence à fronteira.
+    val = 0; cudaMemcpy(dist_dev, &val, sizeof(uint32_t), HOS2DEV); // dist[0] = 0
+    val = 1; cudaMemcpy(fron_dev, &val, sizeof(uint32_t), HOS2DEV); // fron[0] = 1
+}
+
+
+/*
+    Roda BFS calculando vetor de distâncias
+*/
+void calculate_bfs(uint32_t *dist_hos, uint32_t *g_vert_dev, uint32_t *g_list_dev, int vert_n, int edge_n){
+
+    // Aloca e inicializa vetores de distância, vértices processados e fronteira na GPU
+    uint32_t *dist_dev = new_device_array(vert_n);
+    uint32_t *proc_dev = new_device_array(vert_n);
+    uint32_t *fron_dev = new_device_array(vert_n);
+    initialize_aux_arrays(dist_dev, proc_dev, fron_dev, vert_n);
+
+    // Inicializa variáveis auxiliares
+    int n_blocks = (vert_n + THREADS_PER_BLOCK-1) / THREADS_PER_BLOCK;
+    uint32_t *ended_dev = new_device_array(1);
+    uint32_t ended_hos = 0;
+    
+    // Processa vértices até não haver ninguém na fronteira
+    if (LOG) printf("%d blocos de %d threads.\n", n_blocks, THREADS_PER_BLOCK);
+    while (!ended_hos){
+        advance_frontier<<<n_blocks, THREADS_PER_BLOCK>>>(
+            g_vert_dev, g_list_dev, 
+            dist_dev, proc_dev, fron_dev, 
+            vert_n+1, edge_n*2 + 1, ended_dev);
+        cudaDeviceSynchronize();
+        
+        copy_mem(&ended_hos, ended_dev, 1, DEV2HOS); // Copia variável 
+    }
+    copy_mem(dist_hos, dist_dev, vert_n, DEV2HOS);
+
+    free_device_array(dist_dev);
+    free_device_array(proc_dev);
+    free_device_array(fron_dev);
 }
 
 /*
@@ -183,6 +258,8 @@ int main(int argc, char *argv[])
     std::vector<std::pair<int, int>> edges;
     int vert_n = 0, edge_n;
     int u, v;
+
+    // Leitura de entrada
     while (std::cin >> u >> v){
         edges.push_back({u, v});
         vert_n = std::max(vert_n, u);
@@ -191,59 +268,19 @@ int main(int argc, char *argv[])
     vert_n++;
     edge_n = edges.size();
 
-    uint32_t *g_vert_dev, *g_list_dev;
-    gen_dev_graph(edges, &g_vert_dev, &g_list_dev, vert_n);
-
-    uint32_t *g_vert_hos = new_host_array(vert_n+1);
-
-
-    uint32_t *dist = new_device_array(vert_n);
-    uint32_t *proc = new_device_array(vert_n);
-    uint32_t *fron = new_device_array(vert_n);
-    uint32_t one = 1;
-
-    cudaMemset(dist, 0xff, vert_n*sizeof(uint32_t));
-    cudaMemset(proc, 0, vert_n*sizeof(uint32_t));
-    cudaMemset(fron, 0, vert_n*sizeof(uint32_t));
-
-    cudaMemset(dist, 0, sizeof(uint32_t)); // dist[0] = 0;
-    cudaMemcpy(fron, &one, sizeof(uint32_t), HOS2DEV);
-
-    int n_blocks = (vert_n + THREADS_PER_BLOCK-1) / THREADS_PER_BLOCK;
-    uint32_t *ended_dev = new_device_array(1);
-
-    //printf("%d blocos de %d threads.\n", n_blocks, THREADS_PER_BLOCK);
-
-    uint32_t ended_hos = 0;
-    uint32_t *fron_hos = new_host_array(vert_n);
-    int cnt = 0;
-    while (!ended_hos && cnt < 10){
-        cnt++;
-
-        process_frontier<<<n_blocks, THREADS_PER_BLOCK>>>(
-            g_vert_dev, g_list_dev, 
-            dist, proc, fron, 
-            vert_n+1, edges.size()*2 + 1, ended_dev);
-        
-        cudaDeviceSynchronize();
-        
-        copy_mem(&ended_hos, ended_dev, 1, DEV2HOS);
-        copy_mem(fron_hos, dist, vert_n, DEV2HOS);
-
-        //printf("Cabou ? %s\n", ended_hos ? "Sim" : "Não");
- //       print_array(fron_hos, vert_n);
-    }
+    // Aloca e inicializa grafo na GPU a partir do vetor de arestas
+    uint32_t *g_vert_dev = new_device_array( vert_n+1 );
+    uint32_t *g_list_dev = new_device_array( edge_n*2+1 );
+    gen_dev_graph(edges, g_vert_dev, g_list_dev, vert_n, edge_n);
     
-    for (int i = 0; i < vert_n; ++i){
-      printf("%d\n", fron_hos[i]);
-    }
+    // Aloca vetor de distâncias roda BFS 
+    uint32_t *dist_hos = new_host_array(vert_n);
+    calculate_bfs(dist_hos, g_vert_dev, g_list_dev, vert_n, edge_n);
 
-    //printf("Cont: %d\n", cnt);
+    // Libera memória
+    free_host_array(dist_hos);
     free_device_array(g_vert_dev);
     free_device_array(g_list_dev);
-    free_device_array(dist);
-    free_device_array(proc);
-    free_device_array(fron);
 
     return 0;
 }
